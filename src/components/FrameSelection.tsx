3e7d11167a66fabf8aa1267e502cc03c46a43136
kimeunchan <kec4489@naver.com>
2026-01-14 22:29:42 +0900

    해더바 수정

--- src/components/FrameSelection.tsx
+++ src/components/FrameSelection.tsx
@@ -33,12 +33,6 @@ export class FrameSelection {
             <img class="header-logo" src="${aiIconUrl}" alt="promphoto logo" />
               <span>promphoto</span>
           </div>
-
-          <ul class="navbar-menu">
-            <li><a href="#home">홈</a></li>
-            <li><a href="#select">선택</a></li>
-            <li><a href="#image">이미지</a></li>
-          </ul>
         </nav>
         <div class="frame-selection-layout">
 
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -17,12 +17,6 @@ export class MainPage {
               <img class="header-logo" src="${aiIconUrl}" alt="promphoto logo" />
               <span>promphoto</span>
             </div>
-  
-            <ul class="navbar-menu">
-              <li><a href="#home">홈</a></li>
-              <li><a href="#select">선택</a></li>
-              <li><a href="#image">이미지</a></li>
-            </ul>
           </nav>
   
           <div class="main-hero">
